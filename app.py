4d745b2965c184a36a6cbfcce6588ab3d28396ff
aneziac <nateannau@gmail.com>
2020-07-29 21:18:35 -0700

    some more progress

--- app.py
+++ app.py
@@ -10,51 +10,55 @@ from levels import level1 as level1
 
 # Classes
 class World:
-    def __init__(self):
+    def __init__(self, generated=True):
         self.TILE_SIZE = 64
         self.TREE_HEIGHT = 600
         self.TREE_WIDTH = round(self.TREE_HEIGHT * (100 / 64))
         self.TREE_OVERLAP = self.TREE_WIDTH // 2
-        self.TREE_DIST = self.TREE_WIDTH - self.TREE_OVERLAP
-        self.BACKGROUND_WIDTH = math.floor(screen.WIDTH / self.TREE_DIST) + 1
+        self.TREE_DIST = self.TREE_WIDTH - self.TREE_OVERLAP # effective width of trees
+        self.BACKGROUND_WIDTH = (screen.WIDTH // self.TREE_DIST) + 2
+        self.RELATIVE_TREE_SPEED = 3
 
-        self.SCREEN_TILE_WIDTH = math.floor(screen.WIDTH / self.TILE_SIZE) + 1
-        self.SCREEN_TILE_HEIGHT = math.floor(screen.HEIGHT / self.TILE_SIZE) + 1
+        self.SCREEN_TILE_WIDTH = (screen.WIDTH // self.TILE_SIZE) + 1
+        self.SCREEN_TILE_HEIGHT = (screen.HEIGHT // self.TILE_SIZE) + 1
 
-        self.HEIGHT = self.SCREEN_TILE_HEIGHT
-        self.WIDTH = 100
+        if generated:
+            self.HEIGHT = self.SCREEN_TILE_HEIGHT
+            self.WIDTH = 100
+            self.create_map()
+        else:
+            self.HEIGHT = len(level1.world)
+            self.WIDTH = len(level1.world[0])
+            self.MAP = level1.world[::-1]
 
         assert self.HEIGHT >= self.SCREEN_TILE_HEIGHT
         assert self.WIDTH >= self.SCREEN_TILE_WIDTH
 
         self.TILE_TEXTURES = pglib.load_folder("tile", [self.TILE_SIZE] * 2)
         self.TREE_TEXTURES = pglib.load_folder("tree", [self.TREE_WIDTH, self.TREE_HEIGHT])
-        self.create_map()
+
+        self.BACKGROUND_SCALAR = (self.TREE_MAX / self.WIDTH) / self.RELATIVE_TREE_SPEED
 
     def create_map(self):
-        self.TREE_MAX = round((self.WIDTH * self.TILE_SIZE) / self.TREE_DIST)
-        self.TREES = [0] * self.TREE_MAX
+        self.TREE_MAX = (self.WIDTH * self.TILE_SIZE) // self.TREE_DIST
+        self.TREES = []
         for x in range(self.TREE_MAX):
-            self.TREES[x] = randint(1, pglib.num_files("./assets/image/tree")) - 1
+            self.TREES.append(randint(1, pglib.num_files("./assets/image/tree")) - 1)
 
         self.MAP = [[0] * self.WIDTH for _ in range(self.HEIGHT)]
-        #self.MAP = level1.world[::-1]
-        #self.WORLD_WIDTH = len(level1.world[0])
 
         # add grass and water
         for x in range(self.WIDTH):
             self.MAP[0][x] = 1
-        # for x in range(randint(self.WORLD_WIDTH // 20, self.WORLD_WIDTH // 10)):
-        #    self.MAP[]
 
     def render_trees(self):
-        background_scroll_x = player.scroll_x #* (world.SCREEN_TILE_WIDTH / world.TREE_WIDTH)
+        background_scroll_x = player.scroll_x * self.BACKGROUND_SCALAR
         background_tile_offset = background_scroll_x - math.floor(background_scroll_x)
 
         for x in range(self.BACKGROUND_WIDTH):
             tree = self.TREES[x + (math.floor(background_scroll_x))]
             if tree != 0:
-                screen.blit(self.TREE_TEXTURES[tree - 1], [(x - background_tile_offset) * world.TREE_DIST, world.TILE_SIZE - 100])
+                screen.blit(self.TREE_TEXTURES[tree - 1], [(x - background_tile_offset) * self.TREE_DIST, self.TILE_SIZE - 100])
 
     def render_tiles(self):
         x_tile_offset = player.scroll_x - math.floor(player.scroll_x)
@@ -83,36 +87,39 @@ class Player(pg.sprite.Sprite):
         self.YACC = -1
         self.yvel = 0
         self.RESTING_Y = world.TILE_SIZE
-        self.X = world.TILE_SIZE * 2
         self.y = self.RESTING_Y
+
+        self.X = world.TILE_SIZE * 2
 
     def update(self, keys):
         self.tile_x, self.tile_y = math.floor(self.scroll_x), self.y // world.TILE_SIZE
         on_tile = world.MAP[self.tile_y][self.tile_x]
-        below_tile = world.MAP[self.tile_y - 1][self.tile_x]
 
-        print(self.y, below_tile)
         if on_tile == 2:
             self.score += 1
             world.MAP[self.tile_y][self.tile_x] = 0
 
-        if not (below_tile == 5 or below_tile == 1):
+        if not (on_tile == 6 or on_tile == 1):
             if keys[pg.K_SPACE]:
                 if self.costume != [[self.glide[2]], 1]:
                     self.costume = [self.glide, 3]
+                self.yvel = 0
                 self.y -= 2
                 return True
             else:
                 self.yvel += self.YACC
                 if self.costume != [[self.jumpup[6]], 1]:
                     self.costume = [self.jumpup, 7]
-        elif keys[pg.K_w]:
+        elif keys[pg.K_w] and not keys[pg.K_SPACE]:
             self.yvel = 20
         else:
-            if below_tile == 3 or below_tile == 4 or below_tile == 6:
+            if on_tile == 7 or on_tile == 4 or on_tile == 5:
                 return False
 
             self.yvel = 0
+
+            # prevent clipping into tile underneath
+            self.y += world.TILE_SIZE - (self.y % world.TILE_SIZE) - 1
             self.costume = [self.walk, 12]
         self.y += self.yvel
 
@@ -181,4 +188,4 @@ while screen.update():
     # if screen.frame % 5 == 0 and random() < 0.03:
     #    Acorn(randint(round(player.scroll_x + 200), round(player.scroll_x) + screen.WIDTH - 100))
 
-    player.scroll_x += 0.01
+    player.scroll_x += 0.05
